74d4f3fcdeb2b4321fbf49e0dc611a984d3dcdd1
gavin-lightwine <59846949+gavin-lightwine@users.noreply.github.com>
2021-02-16 02:43:22 -0600

    k

--- megatravel/welcome.js
+++ megatravel/welcome.js
@@ -1,40 +1,52 @@
-function Greeting(){
+//java script function to welcome message and picture clock function
+function Greeting() {
+    // getting the time 
     var myDate = new Date();
-    var hrs = myDate.getHours();
-    var greet;
-
-    if (hrs < 12)
-              greet = 'Good Morning';
-            else if (hrs >= 12 && hrs <= 17)
-              greet = 'Good Afternoon';
-             else if (hrs >= 17 && hrs <= 24)
-             greet = 'Good Evening';
-            document.getElementById('greeting').innerHTML =
-        '<b>' + greet + '</b>' + ' the current date and time is ';
-        document.getElementById("time").innerHTML = d;
-
-    
-    return (
-        React.createElement("h3", {id: "Welcome"}, greet, "! ", React.createElement("p", {id: "WelcomeTime"}),"The time is now " + hrs));
-        
-}
-
-function Icon() {
-    var current = new Date();
-    var day_night=current.getHours();
-
-    if (day_night<=12)
-    document.write("<img src='Images/sun.jpg'>")
-    else
-    document.write("<img src='Images/moon.jpg'>")
+    var hours = myDate.getHours();
+    var minutes = myDate.getMinutes();
     
     
-
-}
-
-function Tick() {
-    ReactDOM.render(React.createElement("div", null, React.createElement(Greeting, null)), 
-    document.getElementById("WelcomeBar"));
+    var welcome;
+    // this is for the greeting of the day
+    if (hours < 12)
+            welcome = 'Good Morning';
+    else if (hours >= 12 && hours <= 17)
+            welcome = "Good Afternoon";
+    else if (hours > 17 && hours < 24)
+            welcome = "Good Evening";
+            document.getElementById('Greeting').innerHTML = '<b>'
+            + welcome + '</b>' + 'The current date and time is';
+            document.getElementById("time").innerHTML = d;
     
-}
-setInterval(Tick, 1000); 
+            return (
+                React.createElement("h3", {id: "Welcome"}, greet, "! ", 
+                React.createElement("p", {id: "WelcomeTime"}),"The time is now " + hours + minutes));
+    
+    }
+    //this is for the date and time portion
+    function Time(){
+        var myDate = new Date();
+        let hours = myDate.getHours();
+        let minutes = myDate.getMinutes();
+        let seconds = myDate.getSeconds();
+    
+    }
+    
+    // this is for the image
+    function Icon() {
+        var current = new Date();
+        var dayandnight = current.getHours();
+    if  (hours  >= 6 && hours <= 18){
+        document.write("<img src = 'Images/sun.jpg'>")
+    }
+    else if (hours > 18 && hours < 6){
+    
+        document.write("img src = 'Images/moon.jpg.>");
+    }
+    }
+    function Tick() {
+        ReactDOM.render(React.createElement("div",null,React.createElement(Greeting, null)),
+        document.getElementById("WelcomeBar"));
+    }
+    //updating the clock
+    setInterval(Tick, 1000);
