0d92daf58ca8fe2d8c84d35d6db05306d326145a
gavin-lightwine <59846949+gavin-lightwine@users.noreply.github.com>
2021-03-07 04:03:01 -0600

    Update form.js

--- megatravel/form.js
+++ megatravel/form.js
@@ -13,7 +13,28 @@ function validateForm() {
     }
     var z = document.forms["form1"]["phoneNo"].value;
     if (z ==null ||z == "") {
-      alert("Last Name must be filled out");
+      alert("Phone Number must be filled out");
+      return false;
+    }
+    var a = document.forms["form1"]["email"].value;
+    if (a ==null ||a == "") {
+      alert("Email must be filled out");
+      return false;
+    }
+    var b = document.forms["form1"]["noOfAdults"].value;
+    if (b ==null ||b == "") {
+      alert("Number of Adults must be filled out");
+      return false;
+    }
+    var c = document.forms["form1"]["noOfChild"].value;
+    if (c ==null ||c == "") {
+      alert("Number of Adults must be filled out");
+      return false;
+    }
+    var d = document.forms["form1"]["travelDates"].value;
+    if (d ==null ||d == "") {
+      alert("Number of Adults must be filled out");
       return false;
     }
 }
+
