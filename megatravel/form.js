5eed97a7bc4ecee9bb2f2b8a1dba443b7b1b90c9
gavin-lightwine <59846949+gavin-lightwine@users.noreply.github.com>
2021-03-07 03:08:24 -0600

    Update form.js

--- megatravel/form.js
+++ megatravel/form.js
@@ -1,7 +1,14 @@
 function validateForm() {
     var x = document.forms["form1"]["FirstName"].value;
     if (x == "") {
-      alert("Name must be filled out");
+      alert("First Name must be filled out");
+      return false;
+    }
+  }
+  function validateForm() {
+    var x = document.forms["form1"]["LastName"].value;
+    if (x == "") {
+      alert("Last Name must be filled out");
       return false;
     }
   }
