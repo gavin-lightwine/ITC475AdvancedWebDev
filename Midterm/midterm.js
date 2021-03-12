b830b700d7bdc651648e0c5759857b534ffa4f20
gavin-lightwine <59846949+gavin-lightwine@users.noreply.github.com>
2021-03-11 20:25:13 -0600

    clock

--- Midterm/midterm.js
+++ Midterm/midterm.js
@@ -1,1 +1,11 @@
+function currentTime() {
+    var rtClock = new Date();
+    var hours = rtClock.getHours();
+    var minutes = rtClock.getMinutes();
+    var seconds = rtClock.getSeconds();
 
+
+    document.getElementById('clock').innerHTML = 
+    hours + " : " + minutes + " : " + seconds + " ";
+    var t = setTimeout(currentTime, 500);
+}
