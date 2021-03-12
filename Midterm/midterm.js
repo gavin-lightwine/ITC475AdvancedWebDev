5edab8d6ed07d1558e386d1b4e985b2b6bfc968e
gavin-lightwine <59846949+gavin-lightwine@users.noreply.github.com>
2021-03-12 06:45:02 -0600

    updating

--- Midterm/midterm.js
+++ Midterm/midterm.js
@@ -20,6 +20,54 @@ function getTIme() {
     document.getElementById("time").innerHTML= h + ":" + m;
     var d = setTimeout(currentTime, 1000) 
 
+    document.getElementById('GarageDoorStart').value = time();
+    document.getElementById('GarageDoorEnd').value = time();
+    document.getElementById('LivingRoomLampStart').value = time();
+    document.getElementById('LivingRoomLampEnd').value = time();
+    document.getElementById('FrontDoorStart').value = time();
+    document.getElementById('FrontDoorEnd').value = time();
+    document.getElementById('BackDoorStart').value = time();
+    document.getElementById('BackDoorEnd').value = time();
+    document.getElementById('GarageDoorStart').value = time();
+    document.getElementById('DiningRoomLightStart').value = time();
+    document.getElementById('DiningRoomLightEnd').value = time();
+    document.getElementById('BathroomFanStart').value = time();
+    document.getElementById('BathroomFanEnd').value = time();
+    document.getElementById('KitchenFanStart').value = time();
+    document.getElementById('KitchenFanEnd').value = time();
+    document.getElementById('WasherStart').value = time();
+    document.getElementById('WasherEnd').value = time();
+    document.getElementById('DryerStart').value = time();
+    document.getElementById('DryerEnd').value = time();
+    document.getElementById('GarageDoorStart').value = time();
+    document.getElementById('BedroomDeskLampStart').value = time();
+    document.getElementById('BedroomDeskLampEnd').value = time();
+    document.getElementById('BedroomFloorLampStart').value = time();
+    document.getElementById('BedroomFloorLampEnd').value = time();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+
 
 
 }
