04488505d6ab0fe5d5534fda5440b572089b7ce3
gavin-lightwine <59846949+gavin-lightwine@users.noreply.github.com>
2021-04-11 02:43:24 -0500

    database

--- megatravel/admin.php
+++ megatravel/admin.php
@@ -1,1 +1,38 @@
+<!DOCTYPE html>
 
+
+
+<html>
+ <head>
+  <meta name:"viewport" content="width=device-width, initial-scale=1"/>
+   <title>Mega Travel</title>
+   <link rel="stylesheet" href="style.css"/>
+
+ </head>
+
+  <body>
+   <header>
+    <img src="Images/megatravelLogo.png" height="200px" alt="logo"/>
+ 
+            
+   </header>
+    <section>
+     <div class="bar"></div>
+     <nav>
+      <ul>
+       <li><a href= "index.html">Home</a></li>
+       <li><a href= "about.html">About Us</a></li>
+       <li><a href= "contact.html">Contact Agent</a></li>
+      </ul>
+     </nav>
+
+
+     <footer>
+   <p>Copyright Protected All rights reserved</p><br/>
+   <p>MEGA TRAVELS</p>
+   <p>mega@travels.com</p>
+</footer>
+  
+  </body>
+ 
+</html>
