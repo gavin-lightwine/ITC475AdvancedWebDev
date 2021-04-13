319c2c33c0cc5051d19136bb1f929c153863a1f1
gavin-lightwine <59846949+gavin-lightwine@users.noreply.github.com>
2021-04-13 02:33:20 -0500

    Update admin.php

--- megatravel/admin.php
+++ megatravel/admin.php
@@ -27,15 +27,44 @@
      </nav>
 <?php
 
+
 $host = 'localhost:3306';
 $user = 'gavin';
 $password = "";
+$db = 'megatravel';
 
 
-$conn = mysqli_connect($host,$user,$password);
+$conn =  new mysqli($host,$user,$password, $db);
+
+if (!$conn)
+{
+   echo 'Connection failed<br>';
+   echo 'Error number: ' . mysqli_connect_error() . '<br>';
+   echo 'Error message: ' . mysqli_connect_error() . '<br>';
+   die();
+}
 
 
 
+$sql = "SELECT * FROM `vacation_details`";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+  // output data of each row
+  while($row = $result->fetch_assoc()) {
+    echo "Client First Name:" . $row["FirstName"]. echo "<br>";
+    echo "Client Last Name:" . $row["LastName"]. echo "<br>";
+    echo "Client Phone Number:" . $row["phoneNo"]. echo "<br>";
+    echo "Client Email:" . $row["email"]. echo "<br>";
+    echo "Numbere of Adults:" . $row["NoOfAdults"]. echo "<br>";
+    echo "Number of Children" . $row["noOfChildren"]. echo "<br>";
+    echo "Travel Dates" . $row["travelDates"]. echo "<br>";
+    
+  }
+} else {
+  echo "0 results";
+}
+$conn->close();
 
 
 
