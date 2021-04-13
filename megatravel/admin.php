179203ade98ae08b821cbef8bc96733ecb74f0eb
gavin-lightwine <59846949+gavin-lightwine@users.noreply.github.com>
2021-04-13 02:46:28 -0500

    Update admin.php

--- megatravel/admin.php
+++ megatravel/admin.php
@@ -52,13 +52,13 @@ $result = $conn->query($sql);
 if ($result->num_rows > 0) {
   
   while($row = $result->fetch_assoc()) {
-    echo "Client First Name:"; echo  $row["FirstName"]; echo "<br>";
-    echo "Client Last Name:"; echo $row["LastName"]; echo "<br>";
-    echo "Client Phone Number:"; echo $row["phoneNo"]; echo "<br>";
-    echo "Client Email:"; echo $row["email"]; echo "<br>";
-    echo "Number of Adults:"; echo $row["NoOfAdults"]; echo "<br>";
-    echo "Number of Children:";echo $row["noOfChildren"]; echo "<br>";
-    echo "Travel Dates"; echo $row["travelDates"]; echo "<br>";
+    echo "Client First Name:  "; echo  $row["FirstName"]; echo "<br>";
+    echo "Client Last Name:  "; echo $row["LastName"]; echo "<br>";
+    echo "Client Phone Number:  "; echo $row["phoneNo"]; echo "<br>";
+    echo "Client Email:  "; echo $row["email"]; echo "<br>";
+    echo "Number of Adults:  "; echo $row["NoOfAdults"]; echo "<br>";
+    echo "Number of Children:  ";echo $row["noOfChildren"]; echo "<br>";
+    echo "Travel Dates:  "; echo $row["travelDates"]; echo "<br>";
     
   }
 } else {
