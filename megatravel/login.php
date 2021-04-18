a0f3a76add11236bd4342b6998f1a7cab2e14c1f
gavin-lightwine <59846949+gavin-lightwine@users.noreply.github.com>
2021-04-17 20:32:50 -0500

    login funny error

--- megatravel/login.php
+++ megatravel/login.php
@@ -1,1 +1,51 @@
 
+<!DOCTYPE html>
+
+
+
+<html>
+ <head>
+  <meta name:"viewport" content="width=device-width, initial-scale=1"/>
+   <title>Mega Travel</title>
+   <link rel="stylesheet" href="style.css"/>
+
+ </head>
+
+  <body>
+   <header>
+    <img src="Images/megatravelLogo.png" height="200px" alt="logo"/>
+ 
+            
+   </header>
+    <section>
+     <div class="bar"></div>
+     <nav>
+      <ul>
+       <li><a href= "index.html">Home</a></li>
+       <li><a href= "about.html">About Us</a></li>
+       <li><a href= "contact.html">Contact Agent</a></li>
+      </ul>
+     </nav>
+<body>
+<p>
+    <label>Username:</label>
+    <input type="text" id="user" name:"user" />
+
+
+
+</body>
+     <footer>
+   <p>Copyright Protected All rights reserved</p><br/>
+   <p>MEGA TRAVELS</p>
+   <p>mega@travels.com</p>
+</footer>
+  
+  </body>
+ 
+</html>
+
+
+
+
+
+
