f1e9bc7fd17a037ef7a449e24229bd63fef8666a
gavin-lightwine <59846949+gavin-lightwine@users.noreply.github.com>
2021-04-24 23:00:37 -0500

    Update login.php

--- megatravel/login.php
+++ megatravel/login.php
@@ -4,6 +4,26 @@ $user = 'gavin';
 $password = "";
 $db = 'megatravel';
 
+$conn =  new mysqli($host,$user,$password, $db);
+
+if(isset($_POST['username'])){
+
+  $uname=$_POST['username'];
+  $password=$_POST['password'];
+  
+  $sql="select * from loginform where user='".$username."'AND password='".$password."' limit 1";
+  
+  $result=mysql_query($sql);
+  
+  if(mysql_num_rows($result)==1){
+      echo " You Have Successfully Logged in";
+      exit();
+  }
+  else{
+      echo " You Have Entered Incorrect Password";
+      exit();
+  }
+
 
 
 
