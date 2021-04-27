a75d6196bd891b01a03df7c89b348e4b10b1cab1
gavin-lightwine <59846949+gavin-lightwine@users.noreply.github.com>
2021-04-27 10:34:00 -0500

    Update login.php

--- megatravel/login.php
+++ megatravel/login.php
@@ -5,30 +5,13 @@ $password = "";
 $db = 'megatravel';
 
 $conn = new mysqli($host,$user,$password,$db);
- if ($conn->connect_error) die("Connection died" . $conn->connect_error);
 
- if($_SERVER["REQUEST_METHOD"] == "POST") {
-  
-  $myusername = mysqli_real_escape_string($db,$_POST['username']);
-  $mypassword = mysqli_real_escape_string($db,$_POST['password']); 
-  
-  $sql = "SELECT id FROM login WHERE username = '$username' and passcode = '$password'";
-  $result = mysqli_query($db,$sql);
-  $row = mysqli_fetch_array($result,MYSQLI_ASSOC);
-  $active = $row['active'];
-  
-  $count = mysqli_num_rows($result);
-  
 
-  if($count == 1) {
-     session_register("username");
-     $_SESSION['login_user'] = $username;
-     
-     header("location: welcome.php");
-  }else {
-     $error = "Your Login Name or Password is invalid";
-  }
-}
+
+
+
+
+
 ?>
 
 
